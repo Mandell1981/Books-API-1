d63dd772b17b850021c9c2e0ffd0b989ce61d9ae
Mandell1981 <Mandell1981.rm@gmail.com>
2024-02-10 20:52:08 -0500

    Assignment Complete

--- index.js
+++ index.js
@@ -1,13 +1,31 @@
 require('dotenv').config()
 const express = require('express')
 const app = express()
+const mongoose = require('mongoose')
+const PORT = process.env.PORT
+
+
+// Mongoose
+mongoose.connect(process.env.MONGO_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+})
+
 
 // MIddleware
 app.use(express.json())
-// Route
+// Books
+const booksController = require('./controller/books_controller')
+app.use('/books', booksController)
+
+
+// Home
 app.get('/', (req, res) => {
     res.send('Hello World!')
 })
+
+
+
 
 
 app.listen(process.env.PORT)
